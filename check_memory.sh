09bc7b8b77259f3f949277bc271c3fce5d6935cc
Jamongss <bebemine9700@gmail.com>
2025-10-11 19:37:19 +0900

    Auto-commit at 2025-10-11 19:37:19

--- check_memory.sh
+++ check_memory.sh
@@ -1,6 +1,27 @@
-#!/usr/bin/sh
+#!/usr/bin/env sh
 
-watch -n 1 \
-        'ps auxww --sort=-%mem | awk '\
-        \' 'NR==1{printf "%-10s %-10s %-10s %-10s %-10s %-10s %s\n", $1, $2, $3, $4, "RSS(GB)", $8, $11; next}'\
-        ' {printf "%-10s %-10s %-10s %-10s %-10.2f %-10s %s\n", $1, $2, $3, $4, $6/(1024*1024), $8, $11}'\'' | head -n 40'
+# 메모리 사용량 모니터링 - 내림차순 (Ctrl+C로 종료)
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    while true; do
+        clear
+        ps auxww | sort -k4 -rn | head -n 21 | awk '
+        NR==1{
+            printf "%-10s %-10s %-10s %-10s %-10s %-10s %s\n", "USER", "PID", "%CPU", "%MEM", "RSS(GB)", "STAT", "COMMAND"
+            next
+        }
+        {
+            printf "%-10s %-10s %-10s %-10s %-10.2f %-10s %s\n", $1, $2, $3, $4, $6/(1024*1024), $8, $11
+        }'
+        sleep 1
+    done
+else
+    watch -n 1 'ps auxww --sort=-%mem | awk '\''
+    NR==1{
+        printf "%-10s %-10s %-10s %-10s %-10s %-10s %s\n", $1, $2, $3, $4, "RSS(GB)", $8, $11
+        next
+    }
+    NR<=40{
+        printf "%-10s %-10s %-10s %-10s %-10.2f %-10s %s\n", $1, $2, $3, $4, $6/(1024*1024), $8, $11
+    }'\''
+    '
+fi
